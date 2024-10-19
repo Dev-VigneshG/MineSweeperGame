995ea3edde762d7160735d0d40d751810a8444ca
Vignesh G <71144564+Dev-VigneshG@users.noreply.github.com>
2024-10-19 13:33:52 +0530

    Update MineSweeperGameRunner.java

--- MineSweeperGame/src/runner/MineSweeperGameRunner.java
+++ MineSweeperGame/src/runner/MineSweeperGameRunner.java
@@ -1,3 +1,22 @@
+/*
+ * TITLE:Mine Sweeper Game
+ * AUTHOR:VIGNESH G
+ * DESCRIPTION:
+ *  The main objective of this game is open the cells without clicking bomb
+ *
+ * PACKAGES:
+ * 1)game   -contains game glasses
+ * 2)runner -used to run the Game
+ * 
+ * CLASSES:
+ * 1)MineSweeper(game)       -represents the game
+ * 2)Square(game)              -represent the each cell in the board
+ * 3)MineSweeperGameRunner(runner) -This Class runs the game 
+ * 
+ * TOOL:
+ * 1)Java SE 21
+ * 2)Eclipse IDE
+ */
 package runner;
 import java.util.Scanner;
 import game.MineSweeper;
